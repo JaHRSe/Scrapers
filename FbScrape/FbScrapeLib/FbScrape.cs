00ac18cf2cc489ffa29056cdc6d4cce151a5ab88
Jake <moxiecue.llc@gmail.com>
2024-07-04 12:05:09 -0400

    Initial scrape

---
+++ FbScrape/FbScrapeLib/FbScrape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace FbScrapeLib;
+
+
+public class FbScrape
+{
+
+
+    public static void Test(string url){
+        string className = "x1yztbdb";
+        ChromeOptions options = ConfigureChromeOptions();
+        using (IWebDriver driver = new ChromeDriver(options)){
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+            try{
+                driver.Navigate().GoToUrl(url);
+                wait.Until(drv => drv.FindElement(By.TagName("body")));
+                IWebElement element = driver.FindElement(By.ClassName(className));
+                string text = element.Text;
+                Console.WriteLine(text);
+            } catch (NoSuchElementException ex){
+                Console.WriteLine("Element not found: " + ex.Message);
+            }
+        }
+    }
+
+    private static ChromeOptions ConfigureChromeOptions(){
+        ChromeOptions options = new ChromeOptions();
+        options.AddArgument("--disable-notifications");
+        options.AddArgument("--disable-infobars");
+        options.AddArgument("--start-maximized");
+        options.AddArgument("headless");
+        return options;
+    }
+}
---
+++ FbScrape/ShowCase/Program.cs
@@ -0,0 +1,8 @@
+﻿using FbScrapeLib;
+
+class Program{
+    static void Main(string[] args){
+        string url = "https://www.facebook.com/NintendoAmerica/about";
+        FbScrapeLib.FbScrape.Test(url);
+    }
+}
